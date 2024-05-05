749258261e274e2dae843e03435e763c22fd764a
JosephAJJames <130866326+JosephAJJames@users.noreply.github.com>
2024-05-05 16:57:54 +0000

    Added chef and customer processes

--- main.erl
+++ main.erl
@@ -1,5 +1,19 @@
 -module(main).
--export([start/0]).
+-export([manager/0, customer/1]).
 
-start() ->
-   io:format("~s~n", "Hello, World!").
+customer({DishName, TimeToCook}, Cid) -> %Cid = Pid of a chef process
+   Cid ! {DishName, TimeToCook};
+customer({}) ->
+   {};
+customer(_) ->
+   io:format("~s~p", "Wrong Format").
+
+chef() ->
+   receive
+      _ -> io:format("~s~n", "Recieved Data")
+   end.
+
+manager() ->
+   io:format("~s~n", "Starting..."),
+   Cid = spawn(main, chef, []),
+   spawn(main, customer, [{Pasta, 500}, Cid]).
